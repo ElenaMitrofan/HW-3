38dc0ca7218bb3e6198fa35a027ac967922c6e6e
ElenaMitrofan <114084990+ElenaMitrofan@users.noreply.github.com>
2022-09-23 18:53:16 +0300

    6 задача

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        byte sumOfCans = 120;
-        System.out.println(sumOfCans);
-        byte brownPaintIn1class = 4;
-        System.out.println(brownPaintIn1class);
-        byte whitePaintIn1Class = 2;
-        System.out.println(whitePaintIn1Class);
-        int sumOfCansIn1Class = brownPaintIn1class + whitePaintIn1Class;
-        System.out.println(sumOfCansIn1Class);
-        int sumOfClasses = sumOfCans / sumOfCansIn1Class;
-        System.out.println(sumOfClasses);
-        int brownPaintAll = sumOfClasses * brownPaintIn1class;
-        System.out.println(brownPaintAll);
-        int whitePaintAll = sumOfClasses * whitePaintIn1Class;
-        System.out.println(whitePaintAll);
-        System.out.println("В школе, где " + sumOfClasses + " классов, нужно " + whitePaintAll + " банок белой краски и " + brownPaintAll + " банок коричневой краски");
+        int qtBananas = 5;
+        System.out.println(qtBananas + " шт");
+        int weightAllBananas = qtBananas * 80;
+        System.out.println(weightAllBananas + " гр");
+        int qtMilk = 200;
+        System.out.println(qtMilk + "мл");
+        int PortionOfMilk = qtMilk / 100;
+        System.out.println(PortionOfMilk);
+        int weightAllMilk = 105 * PortionOfMilk;
+        System.out.println(weightAllMilk + " гр");
+        int iceCream = 2;
+        System.out.println(iceCream + " брикета");
+        int weightAllIceCream = iceCream * 100;
+        System.out.println(weightAllIceCream + " гр");
+        int eggs = 4;
+        System.out.println(eggs);
+        int weightAllEggs = eggs * 70;
+        System.out.println(weightAllEggs + " гр");
+        int weightOfDiet = weightAllBananas + weightAllMilk + weightAllIceCream + weightAllEggs;
+        System.out.println(weightOfDiet + " гр");
+
+
 
 
     }
