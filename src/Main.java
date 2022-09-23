8471a105d09c529e22ede59a6ea7b4b79d613df8
ElenaMitrofan <114084990+ElenaMitrofan@users.noreply.github.com>
2022-09-23 18:06:51 +0300

    3 задача

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        double a = 27.12;
-        System.out.println (a);
-        long b = 987678965549L;
-        System.out.println (b);
-        float c = 2.786f;
-        System.out.println (c);
-        boolean d = 3<2;
-        System.out.println (d);
-        short e = 569;
-        System.out.println (e);
-        short f = -159;
-        System.out.println (f);
-        short g = 27897;
-        System.out.println (g);
-        byte h = 67;
-        System.out.println (h);
+        int LP = 23;
+        System.out.println (LP);
+        int AS = 27;
+        System.out.println (AS);
+        int EA = 30;
+        System.out.println (EA);
+        int allStudens = LP + AS + EA;
+        System.out.println (allStudens);
+        int amtPaper = 480 / allStudens;
+        System.out.println ("На каждого ученика рассчитано " + amtPaper + " листов бумаги");
 
 
 
