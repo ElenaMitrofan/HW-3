bc2aaac042fcd4b252facae6b3aa41714d13b57b
ElenaMitrofan <114084990+ElenaMitrofan@users.noreply.github.com>
2022-09-23 18:18:53 +0300

    4 задача

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        int LP = 23;
-        System.out.println (LP);
-        int AS = 27;
-        System.out.println (AS);
-        int EA = 30;
-        System.out.println (EA);
-        int allStudens = LP + AS + EA;
-        System.out.println (allStudens);
-        int amtPaper = 480 / allStudens;
-        System.out.println ("На каждого ученика рассчитано " + amtPaper + " листов бумаги");
+        int bottle = 16;
+        System.out.println(bottle);
+        int time = 2;
+        System.out.println(time);
+        int productivity = bottle / time;
+        System.out.println("продуктивность в минуту составляет " + productivity + " бутылок");
+        int min20 = productivity * 20;
+        System.out.println("за " + "20 минут" + " машины произвела бутылок " + min20 + " штук");
+        int day = productivity * 1440;
+        System.out.println("за " + "сутки" + " машины произвела бутылок " + day + " штук");
+        int days3 = day * 3;
+        System.out.println("за " + "3 дня" + " машины произвела бутылок " + days3 + " штук");
+        int days30 = days3 * 10;
+        System.out.println("за " + "30 дней" + " машины произвела бутылок " + days30 + " штук");
+
+
 
 
 
