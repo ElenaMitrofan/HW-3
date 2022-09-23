3374461a3306e901f781b3b733b08f97013adb75
ElenaMitrofan <114084990+ElenaMitrofan@users.noreply.github.com>
2022-09-23 19:27:53 +0300

    8 задача

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        int goal = 7;
-        System.out.println(goal + " кг");
-        int goalGr = goal * 1000;
-        System.out.println(goalGr + " гр");
-        int goalIn1Day1 = 250;
-        System.out.println(goalIn1Day1 + " гр");
-        int daysToGoal1 = goalGr / goalIn1Day1;
-        System.out.println("для похудения потребуется " + daysToGoal1 + " дней");
-        int goalIn1Day2 = 500;
-        System.out.println(goalIn1Day2 + " гр");
-        int daysToGoal2 = goalGr / goalIn1Day2;
-        System.out.println("для похудения потребуется " + daysToGoal2 + " дней");
+        int misha = 67760;
+        System.out.println("Миша получает " + misha + " р / мес");
+        int mishasAnnualSalaryBefore = misha * 12;
+        System.out.println(mishasAnnualSalaryBefore + " р / год");
+        double mishasSalaryNow = misha * 0.1 + misha;
+        System.out.println(mishasSalaryNow + " р / мес после повышения");
+        double mishasAnnulSalaryNow = mishasSalaryNow * 12;
+        System.out.println(mishasAnnulSalaryNow + " р / год");
+        double mishasDifference = mishasAnnulSalaryNow - mishasAnnualSalaryBefore;
+        System.out.println("Миша теперь получает " + mishasSalaryNow + " рублей. "+ "Годовой доход вырос на " + mishasDifference + " рублей");
+        int denis = 83690;
+        System.out.println("Денис получает " + denis + " р");
+        int denisAnnualSalaryBefore = denis * 12;
+        System.out.println(denisAnnualSalaryBefore + " р / год");
+        double denisSalaryNow = denis * 0.1 + denis;
+        System.out.println(denisSalaryNow + " р / мес после повышения");
+        double denisAnnulSalaryNow = denisSalaryNow * 12;
+        System.out.println(denisAnnulSalaryNow + " р / год");
+        double denisDifference = denisAnnulSalaryNow - denisAnnualSalaryBefore;
+        System.out.println("Денис теперь получает " + denisSalaryNow + " рублей. "+ "Годовой доход вырос на " + denisDifference + " рублей");
+        int kristina = 76230;
+        System.out.println("Кристина получает " + kristina + " р");
+        int kristinasAnnualSalaryBefore = kristina * 12;
+        System.out.println(kristinasAnnualSalaryBefore + " р / год");
+        double kristinasSalaryNow = kristina * 0.1 + kristina;
+        System.out.println(kristinasSalaryNow + " р / мес после повышения");
+        double kristinasAnnulSalaryNow = kristinasSalaryNow * 12;
+        System.out.println(kristinasAnnulSalaryNow + " р / год");
+        double kristinasDifference = kristinasAnnulSalaryNow - kristinasAnnualSalaryBefore;
+        System.out.println("Кристина теперь получает " + kristinasSalaryNow + " рублей. "+ "Годовой доход вырос на " + kristinasDifference + " рублей");
+
+
+
 
 
 
