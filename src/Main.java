6ab54202d21ee123f1fde66de17ad7adc7b4d3b5
ElenaMitrofan <114084990+ElenaMitrofan@users.noreply.github.com>
2022-09-23 15:42:39 +0300

    1 задача

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        int a = 1;
+        System.out.println ("Значение переменной " + " int " + " с типом " + "a" + " равно " + a);
+        byte b = 2;
+        System.out.println ("Значение переменной " + " byte " + " с типом " + "b" + " равно " + b);
+        short c = 3;
+        System.out.println ("Значение переменной " + " short " + " с типом " + "c" + " равно " + c);
+        long d = 4;
+        System.out.println ("Значение переменной " + " long " + " с типом " + "d" + " равно " + d);
+        float e = 5;
+        System.out.println ("Значение переменной " + " float " + " с типом " + "e" + " равно " + e);
+        double f = 6;
+        System.out.println ("Значение переменной " + " double " + " с типом " + "f" + " равно " + f);
+
 
     }
 }
